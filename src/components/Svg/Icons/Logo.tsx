3a54095f1f6d68e4ab5ecdaa438614271a7c50cf
cryptoinhero <33366277+cryptoinhero@users.noreply.github.com>
2022-01-12 22:04:20 +0800

    update spinner

--- src/components/Svg/Icons/Logo.tsx
+++ src/components/Svg/Icons/Logo.tsx
@@ -5,7 +5,7 @@ import { SvgProps } from "../types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 75 75" {...props}>
-      <image height="80" href='/images/LogoWithoutText.png'/>
+      <image height="75" href='/images/LogoWithoutText.png'/>
     </Svg>
     
   );
